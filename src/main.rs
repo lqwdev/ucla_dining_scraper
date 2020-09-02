afba5150a2ec9e395afc8936f353fdaeee95af1c
QINGWEI LAN <qingweilan@gmail.com>
2020-09-01 22:20:56 -0500

    refactor fetch as independent function

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,21 @@ mod model;
 mod parse;
 mod request;
 
-use model::Menu;
 use request::Request;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let requests = request::menu_requests();
     for request in requests {
-        fetch_and_print(&request).await?
+        if let Ok(body) = fetch(&request).await {
+            let menu = parse::parse(body.as_str(), &request);
+            println!("{}", menu)
+        }
     }
-
     Ok(())
 }
 
-async fn fetch_and_print(request: &Request) -> Result<(), Box<dyn std::error::Error>> {
-    let url = request.url();
-    println!("Fetching: {}", url);
-
-    let menu = fetch_and_parse(request).await?;
-    println!("{}", menu);
-
-    Ok(())
-}
-
-async fn fetch_and_parse(request: &Request) -> Result<Menu, Box<dyn std::error::Error>> {
+async fn fetch(request: &Request) -> Result<String, Box<dyn std::error::Error>> {
     let body = reqwest::get(request.url().as_str()).await?.text().await?;
-    let page = parse::parse(body.as_str(), &request);
-    Ok(page)
+    Ok(body)
 }
