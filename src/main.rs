0baf8d2cbb0afbdbd78ca8dd19c441d4ea53f083
QINGWEI LAN <qingweilan@gmail.com>
2020-09-20 20:42:27 -0500

    [cli] support command line arguments

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,43 @@ mod model;
 mod parse;
 mod request;
 
+use clap::{App, Arg, ArgMatches};
 use request::Request;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let requests = request::menu_requests();
+    let app = App::new("UCLA Menu Scraper")
+        .version("1.0.0")
+        .author("Qingwei Lan <qingweilandeveloper@gmail.com>")
+        .about("Scrapes UClA dining website for menus and downloads the data")
+        .arg(
+            Arg::with_name("print")
+                .short("p")
+                .long("print")
+                .help("Print the downloaded menus content"),
+        )
+        .arg(
+            Arg::with_name("all")
+                .short("a")
+                .long("all")
+                .help("Download all menus starting from current date")
+                .conflicts_with("date"),
+        )
+        .arg(
+            Arg::with_name("date")
+                .long("date")
+                .required_unless("all")
+                .takes_value(true)
+                .help("Specify the date (YYYY-MM-DD) for which menu to download"),
+        )
+        .get_matches();
+
+    run(&app).await
+}
+
+async fn run(app: &ArgMatches<'_>) -> Result<(), Box<dyn std::error::Error>> {
+    let requests = parse_requests(app);
+
     for request in requests {
         print!(
             "{} {} for {} ... \t",
@@ -15,15 +47,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             request.meal.name(),
             request.restaurant.name()
         );
+
         if let Ok(body) = fetch(&request).await {
-            let _menu = parse::parse(body.as_str(), &request);
+            let menu = parse::parse(body.as_str(), &request);
             println!("[done]");
+
+            if should_print(&app) {
+                println!("{}", menu);
+            }
         }
     }
+
     Ok(())
 }
 
 async fn fetch(request: &Request) -> Result<String, Box<dyn std::error::Error>> {
     let body = reqwest::get(request.url().as_str()).await?.text().await?;
     Ok(body)
+}
+
+fn parse_requests(app: &ArgMatches) -> Vec<Request> {
+    if app.is_present("all") {
+        return request::menu_requests();
+    }
+
+    let date = app.value_of("date").unwrap();
+    return request::menu_requests_for_dates(vec![date.into()]);
+}
+
+fn should_print(app: &ArgMatches) -> bool {
+    app.is_present("print")
 }
--- src/request.rs
+++ src/request.rs
@@ -25,7 +25,11 @@ pub fn menu_requests() -> Vec<Request> {
     menu_requests_for_dates(date::dates())
 }
 
-fn menu_requests_for_dates(dates: Vec<String>) -> Vec<Request> {
+pub fn menu_requests_for_dates(dates: Vec<String>) -> Vec<Request> {
+    for date in &dates {
+        verify_date(date);
+    }
+
     Restaurant::iter()
         .cartesian_product(dates)
         .cartesian_product(Meal::iter())
@@ -38,6 +42,44 @@ fn menu_request(date: String, restaurant: Restaurant, meal: Meal) -> Request {
         date: date,
         restaurant: restaurant,
         meal: meal,
+    }
+}
+
+/// Verify that the date string is in correct YYYY-MM-DD format
+fn verify_date(date: &String) {
+    fn report_invalid_date(date: &String) {
+        panic!("Given date must be in YYYY-MM-DD format, got {}", date);
+    }
+
+    fn is_numeric(s: &str) -> bool {
+        for c in s.to_string().chars() {
+            if !c.is_numeric() {
+                return false;
+            }
+        }
+        true
+    }
+
+    let tokens = date.split("-").collect::<Vec<&str>>();
+
+    // Must have 3 tokens, a token for each date component
+    if tokens.len() != 3 {
+        report_invalid_date(date);
+    }
+
+    // Verify the year component
+    if tokens[0].len() != 4 || !is_numeric(tokens[0]) {
+        report_invalid_date(date);
+    }
+
+    // Verify the month component
+    if tokens[1].len() != 2 || !is_numeric(tokens[1]) {
+        report_invalid_date(date);
+    }
+
+    // Verify the day component
+    if tokens[2].len() != 2 || !is_numeric(tokens[2]) {
+        report_invalid_date(date);
     }
 }
 
